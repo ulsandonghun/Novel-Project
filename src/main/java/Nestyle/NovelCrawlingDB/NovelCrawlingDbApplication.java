75a3e20a95e3b90567d0a03e32c491d7d820b8c4
ulsandonghun <donghoon141@naver.com>
2024-01-08 14:09:37 +0900

    [setting] : first Setting

---
+++ src/main/java/Nestyle/NovelCrawlingDB/NovelCrawlingDbApplication.java
@@ -0,0 +1,13 @@
+package Nestyle.NovelCrawlingDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovelCrawlingDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovelCrawlingDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Nestyle/NovelCrawlingDB/NovelCrawlingDbApplicationTests.java
@@ -0,0 +1,13 @@
+package Nestyle.NovelCrawlingDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovelCrawlingDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
